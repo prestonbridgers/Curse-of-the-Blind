f2b5afa132ceb4e8ba5dea35a4f37f5c48b46815
Curt Bridgers <bridgerspc@appstate.edu>
2019-10-24 23:45:00 -0400

    Wrote story

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ncurses.h>
+#include <unistd.h>
 #include "player.h"
 #include "map.h"
 #include "event.h"
@@ -12,7 +13,8 @@
 // | |/ _ \ __|// __| | '_ \| |/ _` | | | | / __|/ _ \| '_ ` _ \ / _ \
 // | |  __/ |_  \__ \ | |_) | | (_| | |_| | \__ \ (_) | | | | | |  __/
 // |_|\___|\__| |___/ | .__/|_|\__,_|\__, | |___/\___/|_| |_| |_|\___|
-//                    |_|            |___/                            //   __            _    _                     _     _            
+//                    |_|            |___/                            
+//   __            _    _                     _     _            
 //  / _|_   _  ___| | _(_)_ __   __ _  __   _(_) __| | ___  ___  
 // | |_| | | |/ __| |/ / | '_ \ / _` | \ \ / / |/ _` |/ _ \/ _ \
 // |  _| |_| | (__|   <| | | | | (_| |  \ V /| | (_| |  __/ (_) |
@@ -59,10 +61,9 @@ int main(int argc, char *argv[])
 
 	// Vars
 	struct GAME *game = malloc(sizeof(struct GAME));
-	game->map_win = map_newwin("map.txt");
+	game->map_win = map_newwin("maps/map.txt");
 	game->is_running = 1;
 	map_show(game->map_win);
-
 
 	// GAME LOOP
 	char in;
@@ -94,31 +95,31 @@ void event_handle(struct GAME *g, char input)
 		case 'k': // up
 			if (y - 1 < 0) break;
 			else if (g->map_win->map->data[y - 1][x] == '#') break;
-			else if (g->map_win->map->data[y - 1][x] == 'T') menus_display_note("note.txt");
+			else if (g->map_win->map->data[y - 1][x] == 'T') menus_display_note("tapes/note.txt");
 			else g->map_win->map->pc->ypos--;
 			break;
 		case 'j': // down
 			if (y + 1 > g->map_win->map->height - 1) break;
 			else if (g->map_win->map->data[y + 1][x] == '#') break;
-			else if (g->map_win->map->data[y + 1][x] == 'T') menus_display_note("note.txt");
+			else if (g->map_win->map->data[y + 1][x] == 'T') menus_display_note("tapes/note.txt");
 			else g->map_win->map->pc->ypos++;
 			break;
 		case 'h': // left
 			if (x - 1 < 0) break;
 			else if (g->map_win->map->data[y][x - 1] == '#') break;
-			else if (g->map_win->map->data[y][x - 1] == 'T') menus_display_note("note.txt");
+			else if (g->map_win->map->data[y][x - 1] == 'T') menus_display_note("tapes/note.txt");
 			else g->map_win->map->pc->xpos--;
 			break;
 		case 'l': // right
 			if (x + 1 > g->map_win->map->width - 1) break;
 			else if (g->map_win->map->data[y][x + 1] == '#') break;
-			else if (g->map_win->map->data[y][x + 1] == 'T') menus_display_note("note.txt");
+			else if (g->map_win->map->data[y][x + 1] == 'T') menus_display_note("tapes/note.txt");
 			else g->map_win->map->pc->xpos++;
 			break;
 		case 'y': // up-left
 			if (x - 1 < 0 && y - 1 < 0) break;
 			else if (g->map_win->map->data[y - 1][x - 1] == '#') break;
-			else if (g->map_win->map->data[y - 1][x - 1] == 'T') menus_display_note("note.txt");
+			else if (g->map_win->map->data[y - 1][x - 1] == 'T') menus_display_note("tapes/note.txt");
 			else 
 			{
 				g->map_win->map->pc->xpos--;
@@ -128,7 +129,7 @@ void event_handle(struct GAME *g, char input)
 		case 'u': // up-right
 			if (x + 1 < 0 && y - 1 < 0) break;
 			else if (g->map_win->map->data[y - 1][x + 1] == '#') break;
-			else if (g->map_win->map->data[y - 1][x + 1] == 'T') menus_display_note("note.txt");
+			else if (g->map_win->map->data[y - 1][x + 1] == 'T') menus_display_note("tapes/note.txt");
 			else 
 			{
 				g->map_win->map->pc->xpos++;
@@ -138,7 +139,7 @@ void event_handle(struct GAME *g, char input)
 		case 'b': // down-left
 			if (x - 1 < 0 && y + 1 < 0) break;
 			else if (g->map_win->map->data[y + 1][x - 1] == '#') break;
-			else if (g->map_win->map->data[y + 1][x - 1] == 'T') menus_display_note("note.txt");
+			else if (g->map_win->map->data[y + 1][x - 1] == 'T') menus_display_note("tapes/note.txt");
 			else 
 			{
 				g->map_win->map->pc->xpos--;
@@ -148,7 +149,7 @@ void event_handle(struct GAME *g, char input)
 		case 'n': // down-right
 			if (x + 1 < 0 && y + 1 < 0) break;
 			else if (g->map_win->map->data[y + 1][x + 1] == '#') break;
-			else if (g->map_win->map->data[y + 1][x + 1] == 'T') menus_display_note("note.txt");
+			else if (g->map_win->map->data[y + 1][x + 1] == 'T') menus_display_note("tapes/note.txt");
 			else 
 			{
 				g->map_win->map->pc->xpos++;
